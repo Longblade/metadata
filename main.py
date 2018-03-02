819eba448771c0d4e39947c77e9a17c6b1426ea3
laptop <maxpapkov@mail.ru>
2018-03-02 03:23:51 +0300

    Fixed main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import xml.dom.minidom as md
 import os
-from utils.XML_to_RAM_conversion import XMLToRAMTranslator
+from utils.XML_to_RAM_conversion import XMLToRAMConverter
 
 xml = md.parse(os.path.join("source_xmls/", "TASKS.xml"))
-schema = XMLToRAMTranslator(xml).xml_to_ram()
+schema = XMLToRAMConverter(xml).xml_to_ram()
 print(schema.name)
