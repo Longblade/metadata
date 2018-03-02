d3ebeddbbba86c0558fa5067e038b7b02fb2e013
laptop <maxpapkov@mail.ru>
2018-03-02 04:36:51 +0300

    https://github.com/Longblade/metadata/issues/5

--- main.py
+++ main.py
@@ -3,11 +3,16 @@ import xml.dom.minidom as md
 from utils.XML_to_RAM_conversion import XMLToRAMConverter
 from utils.RAM_to_XML_conversion import RAMToXMLConverter
 from utils.RAM_to_DBD_conversion import RAMToDBDConverter
-from utils.DBD_to_RAM_conversion import  DBDToRAMConverter
+from utils.DBD_to_RAM_conversion import DBDToRAMConverter
 from data_base_sources.DBD_const import SQL_DBD_Init
 
 
 xml = md.parse(os.path.join("source_xmls/", "TASKS.xml"))
 schema = XMLToRAMConverter(xml).xml_to_ram()
+
+xml1 = RAMToXMLConverter(schema).ram_to_xml()
+with open("./tt.xml", "wb") as f:
+    f.write(xml1.toprettyxml(encoding="utf-8", indent="  "))
+
 conv = RAMToDBDConverter(schema, "tasks.db", SQL_DBD_Init).ram_to_dbd()
 schema = DBDToRAMConverter("tasks.db").dbd_to_ram()
