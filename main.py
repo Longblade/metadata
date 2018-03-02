b3795474bca41d43f538db66e0758ac80b322b04
laptop <maxpapkov@mail.ru>
2018-03-02 03:56:30 +0300

    https://github.com/Longblade/metadata/issues/2

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-import xml.dom.minidom as md
 import os
+import xml.dom.minidom as md
 from utils.XML_to_RAM_conversion import XMLToRAMConverter
+from utils.RAM_to_XML_conversion import RAMToXMLConverter
+
 
 xml = md.parse(os.path.join("source_xmls/", "TASKS.xml"))
 schema = XMLToRAMConverter(xml).xml_to_ram()
-print(schema.name)
+xml1 = RAMToXMLConverter(schema).ram_to_xml()
+with open("./tt.xml", "wb") as f:
+    f.write(xml1.toprettyxml(encoding="utf-8", indent="  "))
